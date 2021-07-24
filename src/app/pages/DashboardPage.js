d3ddbea8d422d791309c10f32c424bf3a6bdd121
erfanghayomi <erfuunghayomi@gmail.com>
2021-07-24 15:03:30 +0430

    init project

---
+++ src/app/pages/DashboardPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import {
+//   Dashboard
+// } from "../../_metronic/_partials";
+
+export function DashboardPage() {
+  return (
+    <>
+      dashboard custom
+    </>
+  );
+}
